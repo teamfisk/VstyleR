96acdc206ba6a9e8b1c2b955128f581526563211
ViktorLjung <vilb12@student.bth.se>
2016-05-23 22:49:40 +0200

    LaserPointer base

---
+++ Assets/LaserPointer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserPointer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    Vector3 GetIntersection(){
+        Vector3 position = new Vector3(0, 0, 0);
+        return position;
+    }
+}
